ffbf365aa7e886f39ed787b7fa6df1dd83c6a591
Sherin Binu <sherinbinu@hotmail.com>
2020-07-25 10:06:27 +0530

    setup basic registration module

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000);
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(3000, () => {
+  console.log("running on port 3000");
+});
