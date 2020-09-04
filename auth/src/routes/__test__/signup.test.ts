39d3381cd9708e85f0b6b89055c8e499aed1b1b5
Sherin Binu <sherinbinu@hotmail.com>
2020-09-04 19:07:05 +0530

    add signin and signout test suites

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -26,16 +26,16 @@ it('returns a 400 with an invalid password', async () => {
 })
 
 it('returns a 400 with missing email and password', async () => {
-  await request(app).post('/api/users/signup').send({ email: 'test@gmail.com' }).expect(400)
+  await request(app).post('/api/users/signup').send({ email: 'test@test.com' }).expect(400)
   await request(app).post('/api/users/signup').send({ password: 'password' }).expect(400)
 })
 
 it('disallows duplicate emails', async () => {
-  await request(app).post('/api/users/signup').send({ email: 'test@gmail.com', password: 'password' }).expect(201)
-  await request(app).post('/api/users/signup').send({ email: 'test@gmail.com', password: 'password' }).expect(400)
+  await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201)
+  await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(400)
 })
 
 it('sets a cookie after successful signup', async () => {
-  const response = await request(app).post('/api/users/signup').send({ email: 'test@gmail.com', password: 'password' }).expect(201)
+  const response = await request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201)
   expect(response.get('Set-Cookie')).toBeDefined()
 })
